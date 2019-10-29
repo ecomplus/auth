893decce8c729c410b20d8285173aabd8f37f731
Talisson Ferreira <talissonf@gmail.com>
2019-10-29 19:58:43 -0300

    perf: remove sessionStorage

--- src/methods/logout.js
+++ src/methods/logout.js
@@ -1,13 +1,9 @@
 import emitter from './../lib/emitter'
 
 export default self => {
-  // just clears current session and sessionStorage
+  // just clears current session
   self.session = {}
 
-  window.sessionStorage.removeItem('my_id')
-  window.sessionStorage.removeItem('access_token')
-  window.sessionStorage.removeItem('expires')
-  window.sessionStorage.removeItem('username')
   // emit logout event
   emitter.emit('logout', self)
   return self
