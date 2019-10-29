19ecc836c2cb5a8ce90c294c1e1468a045762125
Talisson Ferreira <talissonf@gmail.com>
2019-10-29 16:22:20 -0300

    feat: setup methods

--- src/index.js
+++ src/index.js
@@ -3,8 +3,32 @@
  * @author E-Com Club <ti@e-com.club>
  * @license AGPL-3.0
  */
+import emitter from './lib/emitter'
+import setSession from './methods/set-session'
+import logout from './methods/logout'
+import login from './methods/login'
+import isLogged from './methods/is-logged'
+import newSession from './methods/admin-session'
+import getAuth from './methods/get-auth'
 
 const EcomAuth = function () {
+  this.session = {}
+
+  const self = this
+
+  this.login = (user, hash) => login(self)(user, hash)
+  this.logout = () => logout(self)
+  this.isLogged = () => isLogged(self)
+  this.setSession = data => setSession(self, data)
+  this.newSession = () => newSession(self)
+  this.getAuth = () => getAuth(self)
 }
+
+// events emitter
+;['on', 'off', 'once'].forEach(method => {
+  EcomAuth[method] = (ev, fn) => {
+    emitter[method](ev, fn)
+  }
+})
 
 export default EcomAuth
