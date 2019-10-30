8117db00a20e851eb1da0449e112b7e25c2d4303
Talisson Ferreira <talissonf@gmail.com>
2019-10-30 14:24:43 -0300

    chore(set-session): set store_id from current login #5

--- src/methods/set-session.js
+++ src/methods/set-session.js
@@ -1,9 +1,10 @@
 import emitter from './../lib/emitter'
-
+import { _config } from '@ecomplus/utils'
 export default (self, session, login) => {
   const { isLogged } = self
   session.session = {}
   session.session = login
+  _config.set('store_id', login.store_id)
   if (isLogged()) {
     // emit login event
     emitter.emit('login', self)
