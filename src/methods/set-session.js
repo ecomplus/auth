783d8578114f32879315e66d2e7aed0627806472
Talisson Ferreira <talissonf@gmail.com>
2019-11-01 13:08:21 -0300

    chore(set-session): update session property correctly

--- src/methods/set-session.js
+++ src/methods/set-session.js
@@ -2,12 +2,15 @@ import emitter from './../lib/emitter'
 import { _config } from '@ecomplus/utils'
 export default (self, session, login) => {
   const { isLogged } = self
-  session.session = {}
-  session.session = login
-  _config.set('store_id', login.store_id)
+
+  session.my_id = login.my_id
+  session.access_token = login.access_token
+  session.store_id = login.storeId
+
+  _config.set('store_id', login.storeId)
   if (isLogged()) {
     // emit login event
-    emitter.emit('login', self)
+    emitter.emit('login', session)
   }
-  return session.session
+  return session
 }
