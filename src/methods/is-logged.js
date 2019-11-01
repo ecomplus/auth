fdd97e6da54c1859c8f515f4300d0fa8dd736a55
Talisson Ferreira <talissonf@gmail.com>
2019-11-01 16:07:24 -0300

    revert: stop using moment js

--- src/methods/is-logged.js
+++ src/methods/is-logged.js
@@ -1,2 +1,1 @@
-import moment from 'moment'
-export default session => (moment().diff(session.expires, 'days') === 0 && Boolean(session.access_token))
+export default session => ((Date.now() < new Date(session.expires).valueOf()) && Boolean(session.access_token))
