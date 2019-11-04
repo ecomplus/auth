7f857c2b5afd3fa8f5d88e6ff3ea1cb7389faea7
Leonardo Matos <leomp120894@gmail.com>
2019-11-04 20:43:17 -0300

    fix(api-request): pass options object to ecomClient.store
    BREAKING CHANGE: changed apiRequest method arguments

--- src/methods/api-request.js
+++ src/methods/api-request.js
@@ -1,16 +1,14 @@
 import { store } from '@ecomplus/client'
 
-export default (self, session, url, method = 'get', data) => {
+export default (self, session, options) => {
   if (!self.isLogged()) {
     return Promise.reject(new Error('Unauthorized, requires login'))
   }
 
   return store({
-    url,
+    ...options,
     storeId: session && session.store_id,
     authenticationId: session && session.my_id,
-    accessToken: session && session.access_token,
-    method,
-    data
+    accessToken: session && session.access_token
   })
 }
