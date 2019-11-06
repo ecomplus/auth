1e56a13407e376ebf394e1cb000c7ab0e2a71f22
Talisson Ferreira <talissonf@gmail.com>
2019-11-06 16:21:29 -0300

    Update src/methods/fetch-authentication.js
    Co-Authored-By: Leonardo Matos <leomp120894@gmail.com>

--- src/methods/fetch-authentication.js
+++ src/methods/fetch-authentication.js
@@ -1,4 +1,7 @@
 export default (self, session) => {
   const url = `/authentications/${session.my_id}.json`
-  return self.apiRequest(url).then(resp => resp.data)
+  return self.apiRequest(url).then(({ data }) => {
+    session.authentication = data
+    return data
+  })
 }
